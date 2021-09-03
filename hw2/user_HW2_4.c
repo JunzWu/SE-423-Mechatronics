08f903f83d4f52744e1dda029c815e4e62af789c
JunzWu <46737744+JunzWu@users.noreply.github.com>
2021-09-02 20:56:27 -0500

    Add files via upload

---
+++ hw2/user_HW2_4.c
@@ -0,0 +1,213 @@
+/******************************************************************************
+MSP430G2553 Project Creator
+
+GE 423  - Dan Block
+        Spring(2017)
+
+        Written(by) : Steve(Keres)
+College of Engineering Control Systems Lab
+University of Illinois at Urbana-Champaign
+*******************************************************************************/
+
+#include "msp430g2553.h"
+#include "UART.h"
+
+char newprint = 0;
+int timecnt = 0;  
+int timecheck = 0;  
+
+int adc_value;
+int count = 0;
+
+void main(void) {
+
+	WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+	if (CALBC1_16MHZ ==0xFF || CALDCO_16MHZ == 0xFF) while(1);
+
+	DCOCTL = CALDCO_16MHZ;    // Set uC to run at approximately 16 Mhz
+	BCSCTL1 = CALBC1_16MHZ; 
+
+	// Initialize Port 1
+	P1SEL &= ~0x10;  // See page 42 and 43 of the G2553's datasheet, It shows that when both P1SEL and P1SEL2 bits are zero
+	P1SEL2 &= ~0x10; // the corresponding pin is set as a I/O pin.
+	P1REN = 0;       // No resistors enabled for Port 1
+	P1DIR |= 0x10;   // Set P1.4 to output to drive LED on LaunchPad board.  Make sure shut jumper is in place at LaunchPad's Red LED
+	P1OUT &= ~0x10;  // Initially set P1.4 to 0
+	P1IFG = 0;
+	P1IES = 0;
+	P1IE = 0;
+
+	// Initialize Port 2 - TA1.1 PWM Output
+	P2DIR |= 0x04;   // P2.2 output
+	P2SEL |= 0x04;   // P2.2 TA1.1 option
+	P2SEL2 &= ~0x04; // P2.2 TA1.1 option
+
+
+	// Timer A Config
+	TACCTL0 = CCIE;          // Enable Periodic interrupt
+	TACCR0 = 16000;          // period = 1ms
+	TACTL = TASSEL_2 + MC_1; // source SMCLK, up mode
+
+	// PWM Config
+	TA1CCR0 = 1600;              // PWM Period
+	TA1CCTL1 = OUTMOD_7;         // TA1CCR1 reset/set
+	TA1CCR1 = 0;                 // TA1CCR1 PWM duty cycle
+	TA1CTL = TASSEL_2 + MC_1;    // SMCLK, up mode
+
+
+	// ADC10 Configuration
+	// Vr+ = Vcc, Vr- = Vss, 8 x ADC10CLKS, ADC10 on, ADC Interrupt enable
+    ADC10CTL0 = SREF_0 + ADC10SHT_1 + ADC10ON + ADC10IE;
+    //Input channel A3, ADC10SC, Clock divide /1, Clock source = ADC10OSC, Single-channel-single-conversion
+    ADC10CTL1 = INCH_3 + SHS_0 + ADC10DIV_0 + ADC10SSEL_0 + CONSEQ_0;
+
+    ADC10AE0 = 0x08;  // Enable A3 ADC channel
+    ADC10CTL0 |= ENC; // ADC10 enabled
+
+
+	Init_UART(9600,1);	// Initialize UART for 9600 baud serial communication
+
+	_BIS_SR(GIE); 		// Enable global interrupt
+
+
+	while(1) {
+
+		if(newmsg) {
+			newmsg = 0;
+		}
+
+		if (newprint)  {
+
+			// P1OUT ^= 0x01; // Blink LED
+
+			TA1CCR1 = (int)((unsigned long)adc_value*1600/1023);
+
+			UART_printf("%dmV %d/1600\n\r", (int)((unsigned long)adc_value*3600/1023), TA1CCR1);
+			// UART_printf("%dmV\n\r", adc_value);
+
+			// adc_value < 200 for 2 seconds turn on LED on P1.4
+			if (adc_value < 200) {
+				count++;
+				if (count >= 8){
+					P1OUT |= 0x10; // turn on LED
+				}
+			}else{
+				count = 0;
+				P1OUT &= ~0x10; // turn off LED
+			}
+
+			newprint = 0;
+		}
+
+	}
+}
+
+
+// Timer A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+{
+	timecheck++; // Keep track of time for main while loop. 
+
+	if (timecheck == 250) {
+	    timecheck = 0;
+	    timecnt++;
+	    ADC10CTL0 |= ENC + ADC10SC; // Trigger ADC10 every 0.25 seconds
+	   // newprint = 1;  // flag main while loop that .25 seconds have gone by.
+	}
+
+}
+
+
+// ADC 10 ISR - Called when a sequence of conversions (A7-A0) have completed
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR(void) {
+
+	adc_value = (int)ADC10MEM; // save conversion value to adc_value
+	newprint = 1;  // flag main while loop that .25 seconds have gone by.
+
+}
+
+
+
+// USCI Transmit ISR - Called when TXBUF is empty (ready to accept another character)
+#pragma vector=USCIAB0TX_VECTOR
+__interrupt void USCI0TX_ISR(void) {
+
+	if(IFG2&UCA0TXIFG) {		// USCI_A0 requested TX interrupt
+		if(printf_flag) {
+			if (currentindex == txcount) {
+				senddone = 1;
+				printf_flag = 0;
+				IFG2 &= ~UCA0TXIFG;
+			} else {
+				UCA0TXBUF = printbuff[currentindex];
+				currentindex++;
+			}
+		} else if(UART_flag) {
+			if(!donesending) {
+				UCA0TXBUF = txbuff[txindex];
+				if(txbuff[txindex] == 255) {
+					donesending = 1;
+					txindex = 0;
+				}
+				else txindex++;
+			}
+		} else {  // interrupt after sendchar call so just set senddone flag since only one char is sent
+			senddone = 1;
+		}
+
+		IFG2 &= ~UCA0TXIFG;
+	}
+
+	if(IFG2&UCB0TXIFG) {	// USCI_B0 requested TX interrupt (UCB0TXBUF is empty)
+
+		IFG2 &= ~UCB0TXIFG;   // clear IFG
+	}
+}
+
+
+// USCI Receive ISR - Called when shift register has been transferred to RXBUF
+// Indicates completion of TX/RX operation
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void) {
+
+	if(IFG2&UCB0RXIFG) {  // USCI_B0 requested RX interrupt (UCB0RXBUF is full)
+
+		IFG2 &= ~UCB0RXIFG;   // clear IFG
+	}
+
+	if(IFG2&UCA0RXIFG) {  // USCI_A0 requested RX interrupt (UCA0RXBUF is full)
+
+	// HW2 Problem 5
+
+//    Uncomment this block of code if you would like to use this COM protocol that uses 253 as STARTCHAR and 255 as STOPCHAR
+/*		if(!started) {	// Haven't started a message yet
+			if(UCA0RXBUF == 253) {
+				started = 1;
+				newmsg = 0;
+			}
+		}
+		else {	// In process of receiving a message		
+			if((UCA0RXBUF != 255) && (msgindex < (MAX_NUM_FLOATS*5))) {
+				rxbuff[msgindex] = UCA0RXBUF;
+
+				msgindex++;
+			} else {	// Stop char received or too much data received
+				if(UCA0RXBUF == 255) {	// Message completed
+					newmsg = 1;
+					rxbuff[msgindex] = 255;	// "Null"-terminate the array
+				}
+				started = 0;
+				msgindex = 0;
+			}
+		}
+*/
+
+		IFG2 &= ~UCA0RXIFG;
+	}
+
+}
+
+
